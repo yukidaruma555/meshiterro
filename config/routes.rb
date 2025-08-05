bc121c0c0d5c3e49b6c5917262038740d96118a8
EC2 Default User <211435376+yukidaruma555@users.noreply.github.com>
2025-08-05 14:51:22 +0000

    [Add] 管理者機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+   
+  devise_for :controllers
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+ 
+ scope module: :public do
   root to: 'homes#top'
   devise_for :users
   get "/homes/about", to: "homes#about", as: 'about'
@@ -7,5 +19,6 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
